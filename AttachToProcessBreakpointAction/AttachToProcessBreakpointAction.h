f79ca195d5c72eda9de8c0be68b469c5ef2b46c9
Michail Pishchagin <mblsha@yandex-team.ru>
2015-02-15 18:06:17 +0300

    Initial Commit

---
+++ AttachToProcessBreakpointAction/AttachToProcessBreakpointAction.h
@@ -0,0 +1,16 @@
+//
+//  AttachToProcessBreakpointAction.h
+//  AttachToProcessBreakpointAction
+//
+//  Created by Michail Pishchagin on 15.02.15.
+//  Copyright (c) 2015 mblsha. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface AttachToProcessBreakpointAction : NSObject
+
++ (instancetype)sharedPlugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
